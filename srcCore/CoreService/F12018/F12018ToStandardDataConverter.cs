beb18f83a14720197397e104c225da74a699a897
David Lusignan <davidl@vuwall.com>
2020-02-07 21:43:21 -0500

    Fix issue where throttle ratio would always be 1 or 0

--- srcCore/CoreService/F12018/F12018ToStandardDataConverter.cs
+++ srcCore/CoreService/F12018/F12018ToStandardDataConverter.cs
@@ -7,7 +7,7 @@ namespace CoreService.F12018 {
     public static class F12018ToStandardDataConverter {
         public static TelemetryState ToTelemetry(F12018Packet packet) {
             var F1Telemetry = (F12018PacketTelemetry)packet.data;
-            var throttleRatio = (float)F1Telemetry.throttle.Value / 100f;
+            var throttleRatio = ((float)F1Telemetry.throttle.Value) / 100f;
             return new TelemetryState(packet.header.sessionTime.Value, throttleRatio, F1Telemetry.speed.Value);
         }
 
