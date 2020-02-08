c411294ef51ede255cc8c50b8ee58ada798aca0b
David Lusignan <davidl@vuwall.com>
2020-02-07 20:29:32 -0500

    Use new standard telemetry data structure

--- srcCore/CoreService/F12018/F12018ToStandardDataConverter.cs
+++ srcCore/CoreService/F12018/F12018ToStandardDataConverter.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace CoreService.F12018 {
     public static class F12018ToStandardDataConverter {
-        public static TelemetryState ToTelemetry(F12018PacketData data) {
-            var F1Telemetry = (F12018PacketTelemetry)data;
+        public static TelemetryState ToTelemetry(F12018Packet packet) {
+            var F1Telemetry = (F12018PacketTelemetry)packet.data;
             var throttleRatio = (float)F1Telemetry.throttle.Value / 100f;
-            return new TelemetryState(throttleRatio, F1Telemetry.speed.Value);
+            return new TelemetryState(packet.header.sessionTime.Value, throttleRatio, F1Telemetry.speed.Value);
         }
     }
 }
