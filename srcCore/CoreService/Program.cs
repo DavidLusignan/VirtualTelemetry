38be16ccdd9208aa58e1ba9e80154fd46a591bf3
David Lusignan <davidl@vuwall.com>
2020-02-07 20:30:29 -0500

    Remove passing of on packet method to F12018PacketHandler

--- srcCore/CoreService/Program.cs
+++ srcCore/CoreService/Program.cs
@@ -5,9 +5,7 @@ using System;
 namespace CoreService {
     class Program {
         static void Main(string[] args) {
-            var handler = new F12018PacketHandler(telemetry => {
-                Console.WriteLine(telemetry);
-            });
+            var handler = new F12018PacketHandler();
             handler.Start();
             while(true){
 
