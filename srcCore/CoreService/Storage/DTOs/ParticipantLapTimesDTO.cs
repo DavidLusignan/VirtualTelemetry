28243abe032e25758dad661b082a55a49cc83bb5
David Lusignan <davidl@vuwall.com>
2020-02-26 20:16:46 -0500

    Create a DTO for ParticipantLapTimes

---
+++ srcCore/CoreService/Storage/DTOs/ParticipantLapTimesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreService.Storage.DTOs {
+    public class ParticipantLapTimesDTO {
+        public Key key { get; private set; }
+        public Guid sessionId;
+        public int participantIndex;
+        public IDictionary<int, ParticipantLapTime> lapTimes;
+
+        public ParticipantLapTimesDTO(Key key, Guid sessionId, int participantIndex, IDictionary<int, ParticipantLapTime> lapTimes) {
+            this.key = key;
+            this.sessionId = sessionId;
+            this.participantIndex = participantIndex;
+            this.lapTimes = lapTimes;
+        }
+    }
+}
