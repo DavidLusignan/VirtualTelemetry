bb779e1497fac14238c1c45b50f1b6085b3c0ed0
David Lusignan <davidl@vuwall.com>
2020-02-26 19:52:03 -0500

    Implement a lap time store (still need to figure out what kind of key I want to be using)

---
+++ srcCore/CoreService/Storage/LapTimeStore.cs
@@ -0,0 +1,39 @@
+﻿using CoreService.Storage.DTOs;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreService.Storage {
+    public interface IStore<T> {
+        IEnumerable<T> LoadAll();
+        void Store(T item);
+        void Update(T item);
+        void Delete(Guid id);
+    }
+
+    public class CollectionStore<T> : IStore<T> {
+        private ILiteCollection<T> _collection;
+        public CollectionStore(LiteDatabase db) {
+            _collection = db.GetCollection<T>();
+        }
+
+        public void Delete(Guid id) {
+            var bytes = id.ToByteArray();
+            var objectId = new ObjectId(bytes, 4);
+            _collection.Delete(objectId);
+        }
+
+        public IEnumerable<T> LoadAll() {
+            return _collection.FindAll();
+        }
+
+        public void Store(T item) {
+            _collection.Insert(item);
+        }
+
+        public void Update(T item) {
+            _collection.Update(item);
+        }
+    }
+}
