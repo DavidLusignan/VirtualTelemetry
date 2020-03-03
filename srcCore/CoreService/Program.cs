da7149be5ffbbd6a9314dcd0721bcc9aeddfdf0b
David Lusignan <davidl@vuwall.com>
2020-03-03 12:59:04 -0500

    Use CollectionStore in PC2LapTimeHandler to store lap times

--- srcCore/CoreService/Program.cs
+++ srcCore/CoreService/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CoreService.F12018;
 using CoreService.ProjectCars2;
+using CoreService.Storage.DTOs;
 using Global.Enumerable;
+using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,17 @@ namespace CoreService {
         static int currentLap = -1;
         static IDictionary<int, float> lapTimes = new Dictionary<int, float>();
         static void Main(string[] args) {
+            DTOBsonConversion.Setup();
             var cache = new LiveDataCache();
             var handler = new PC2PacketHandler();
-            var lapTimeCache = new PC2LapTimeHandler(handler);
+            var db = new LiteDatabase("storage.db");
+            var lapTimeCache = new PC2LapTimeHandler(handler, db);
             handler.Start();
             while(true){
                 Console.WriteLine("Enter to dump");
                 Console.ReadLine();
                 Console.Clear();
-                lapTimeCache.lapTimes.First(lp => lp.participantIndex.Equals(0)).lapTimes.ForEach(lapTime => {
+                lapTimeCache.lapTimes.LoadAll().First(lp => lp.participantIndex.Equals(0)).Entity().lapTimes.ForEach(lapTime => {
                     Console.WriteLine("Lap " + lapTime.Key + "; Total : " + lapTime.Value.lapTime + "; S1: " + lapTime.Value.sector1Time + "; S2: " + lapTime.Value.sector2Time + "; S3: " + lapTime.Value.sector3Time);
                 });
             }
--- srcCore/CoreService/ProjectCars2/PC2LapTimeHandler.cs
+++ srcCore/CoreService/ProjectCars2/PC2LapTimeHandler.cs
@@ -1,7 +1,9 @@
 ﻿using CoreService.Data;
 using CoreService.Storage;
+using CoreService.Storage.DTOs;
 using Global.Enumerable;
 using Global.Observable;
+using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,9 @@ using System.Linq;
 namespace CoreService {
     public class PC2LapTimeHandler {
         private object _stateLock = new object();
-        public IEnumerable<ParticipantLapTimes> lapTimes { get; private set; }
-        public PC2LapTimeHandler(IObservable<DataState> packetHandler) {
-            lapTimes = new List<ParticipantLapTimes>();
+        public CollectionStore<ParticipantLapTimesDTO> lapTimes { get; private set; }
+        public PC2LapTimeHandler(IObservable<DataState> packetHandler, LiteDatabase db) {
+            lapTimes = new CollectionStore<ParticipantLapTimesDTO>(db);
             packetHandler.Subscribe(new Observer<DataState>(OnState));
         }
 
@@ -21,13 +23,13 @@ namespace CoreService {
                     if (newState.dataType.Equals(DataStateType.Time)) {
                         var time = (TimeState)newState;
                         ParticipantLapTimes current;
-                        if (lapTimes.Any(l => l.participantIndex.Equals(time.participantIndex))) {
-                            current = lapTimes.First(l => l.participantIndex.Equals(time.participantIndex));
+                        if (lapTimes.ExistsWhere(l => l.participantIndex.Equals(time.participantIndex))) {
+                            current = lapTimes.FindWhere(l => l.participantIndex.Equals(time.participantIndex)).Entity();
                         } else {
                             current = new ParticipantLapTimes(Key.Create(), time.participantIndex, new Dictionary<int, ParticipantLapTime>());
                         }
                         var updated = current.InsertIfNewTime(time);
-                        lapTimes = lapTimes.Except(current).Concat(updated);
+                        lapTimes.Update(current.DTO());
                     }
                 } catch (Exception e) {
                     Console.WriteLine("Error while updating lap times");
