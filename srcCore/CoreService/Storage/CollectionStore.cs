911847b1f16c9dabc9cd19e0f2241b8f9899dc84
David Lusignan <davidl@vuwall.com>
2020-03-03 12:56:17 -0500

    Create a CollectionStore using litedb

---
+++ srcCore/CoreService/Storage/CollectionStore.cs
@@ -0,0 +1,62 @@
+﻿using CoreService.Storage.DTOs;
+using Global.Enumerable;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CoreService.Storage {
+    public interface IStore<T> {
+        T FindWhere(Func<T, bool> predicate);
+        bool ExistsWhere(Func<T, bool> predicate);
+        IEnumerable<T> LoadAll();
+        void Store(T item);
+        void Update(T item);
+        void Delete(Key key);
+        void DeleteAll();
+    }
+
+    public class CollectionStore<T> : IStore<T> where T : IStorable {
+        private ILiteCollection<T> _collection;
+        public CollectionStore(LiteDatabase db) {
+            _collection = db.GetCollection<T>();
+        }
+
+        public void Delete(Key key) {
+            var bytes = key.AsLiteDB().ToByteArray();
+            var objectId = new ObjectId(bytes, 4);
+            _collection.Delete(objectId);
+        }
+
+        public void DeleteAll() {
+            var all = _collection.FindAll();
+            all.ForEach(doc => _collection.Delete(doc.Id));
+        }
+
+        public IEnumerable<T> LoadAll() {
+            return _collection.FindAll();
+        }
+
+        public bool ExistsWhere(Func<T, bool> predicate) {
+            try {
+                return _collection.FindAll().Any(predicate);
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        public T FindWhere(Func<T, bool> predicate) {
+            return _collection.FindAll().First(predicate);
+        }
+
+        public void Store(T item) {
+            _collection.Insert(item.Id, item);
+        }
+
+        public void Update(T item) {
+            _collection.Upsert(item.Id, item);
+        }
+    }
+}
