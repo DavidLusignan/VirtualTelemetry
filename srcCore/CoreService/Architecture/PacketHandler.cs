c6511aebdaefcee64a4b4592f7ea1019e2afcfe5
David Lusignan <davidl@vuwall.com>
2020-03-03 13:20:21 -0500

    Remove C++ project, will add back if needed later on

---
+++ srcCore/CoreService/Architecture/PacketHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreService.Architecture {
+    interface PC2PacketHandler : IObservable<PC2RawPacket> { }
+    struct PC2RawPacket { }
+
+    interface PC2PacketParser : IObservable<PC2DataPacket> { }
+    struct PC2DataPacket { }
+
+    interface PC2DataIntepretor : IObservable<StdData> { }
+    struct StdData { }
+}
