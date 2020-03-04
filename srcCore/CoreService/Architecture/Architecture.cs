096074844c21a6fc9cd89763c59107b4fc5da110
David Lusignan <davidl@vuwall.com>
2020-03-04 13:30:10 -0500

    Update comments in the Architecture.cs

--- srcCore/CoreService/Architecture/Architecture.cs
+++ srcCore/CoreService/Architecture/Architecture.cs
@@ -4,16 +4,18 @@ using System.Text;
 
 namespace CoreService.Architecture {
     // This is the packet handler, it receives information from an external API in a certain way (usually UDP or shared mem access)
-    // It only reads the bytes of information 
     interface PacketHandler : IObservable<RawPacket> { }
     struct RawPacket { }
 
+    // This is the packet parser, it listens to notifications from the PacketHandler, reads the data coming in and packages it into a packet 
+    // specific to the API
     interface PacketParser : IObservable<DataPacket> { }
     interface PacketFactory {
         DataPacket Create(RawPacket rawPacket);
     }
     struct DataPacket { }
 
+    // This is a data interpretor, it listens to 1..* PacketParser for notifications and creates standard form data notification
     interface DataIntepretor : IObservable<StdData> { }
     interface DataFactory {
         StdData Create(DataPacket dataPacket);
