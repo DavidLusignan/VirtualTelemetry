154590bc57f8cd72c7adb41c4461d0d6556f0b3a
David Lusignan <davidl@vuwall.com>
2020-03-04 21:02:29 -0500

    Observe lap times from main and output to console when a new lap time comes in

--- srcCore/CoreService/Program.cs
+++ srcCore/CoreService/Program.cs
@@ -3,6 +3,7 @@ using CoreService.Storage.DTOs;
 using CoreService.UDPProjectCars2.PacketParser;
 using CoreService.UDPProjectCars2.RawPacketHandler;
 using Global.Enumerable;
+using Global.Observable;
 using LiteDB;
 using System;
 using System.Collections.Generic;
@@ -24,14 +25,17 @@ namespace CoreService {
             var packetParser = new PC2PacketParser(rawHandler);
             var db = new LiteDatabase("storage.db");
             var lapTimeCache = new PC2StdLapTimeConvertor(packetParser, db);
+            lapTimeCache.Subscribe(new Observer<ParticipantLapTimes>(lp => {
+                if (lp.participantIndex == 0) {
+                    Console.Clear();
+                    lp.lapTimes.ForEach(lapTime => {
+                        Console.WriteLine("Lap " + lapTime.Key + "; Total : " + lapTime.Value.lapTime + "; S1: " + lapTime.Value.sector1Time + "; S2: " + lapTime.Value.sector2Time + "; S3: " + lapTime.Value.sector3Time);
+                    });
+                }
+            }));
             rawHandler.Start();
             while(true){
-                Console.WriteLine("Enter to dump");
-                Console.ReadLine();
-                Console.Clear();
-                lapTimeCache.lapTimes.LoadAll().First(lp => lp.participantIndex.Equals(0)).Entity().lapTimes.ForEach(lapTime => {
-                    Console.WriteLine("Lap " + lapTime.Key + "; Total : " + lapTime.Value.lapTime + "; S1: " + lapTime.Value.sector1Time + "; S2: " + lapTime.Value.sector2Time + "; S3: " + lapTime.Value.sector3Time);
-                });
+                
             }
         }
     }
