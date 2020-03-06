2b25c168bdce65416be7e03173746283c80fd40e
David Lusignan <davidl@vuwall.com>
2020-03-05 20:22:33 -0500

    Add missing packet types to PacketType enum

--- srcCore/CoreService/UDPProjectCars2/PacketParser/PC2PacketType.cs
+++ srcCore/CoreService/UDPProjectCars2/PacketParser/PC2PacketType.cs
@@ -1,7 +1,13 @@
 ﻿namespace CoreService.UDPProjectCars2.PacketParser {
     public enum PC2PacketType {
         Telemetry = 0,
+        RaceDefinition = 1,
+        Participants = 2,
         Timings = 3,
-        TimeStats = 7
+        GameState = 4,
+        WeatherState = 5,
+        VehicleNames = 6,
+        TimeStats = 7,
+        ParticipantVehicleNames = 8
     }
 }
