22a6843329adce9b9aebba1913d2ec14155dd836
David Lusignan <davidl@vuwall.com>
2020-03-06 22:13:41 -0500

    Use new entities in main to write updates to console

--- srcCore/CoreService/Program.cs
+++ srcCore/CoreService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CoreService.Data;
 using CoreService.F12018;
+using CoreService.Storage;
 using CoreService.Storage.DTOs;
 using CoreService.UDPProjectCars2.PacketParser;
 using CoreService.UDPProjectCars2.RawPacketHandler;
@@ -8,6 +9,7 @@ using Global.Enumerable;
 using Global.Observable;
 using LiteDB;
 using System;
+using System.Linq;
 
 namespace CoreService {
     class Program {
@@ -17,9 +19,19 @@ namespace CoreService {
             var rawHandler = PC2RawHandler.Create(DEFAULT_PORT);
             var packetParser = new PC2PacketParser(rawHandler);
             var db = new LiteDatabase("storage.db");
-            var test = new PC2SessionIDPipeline(0, packetParser);
-            test.Subscribe(new Observer<SessionState>(session => {
+            var sessionPipeline = new PC2SessionIDPipeline(0, packetParser);
+            var lapTimesStore = new ParticipantLapTimesStore(db);
+            var lapTimesPipeline = new PC2StdLapTimePipeline(packetParser, lapTimesStore);
+            lapTimesStore.Observe(lapTimesPipeline);
+            sessionPipeline.Subscribe(new Observer<SessionState>(session => {
                 Console.WriteLine(session);
+            }));
+            lapTimesPipeline.Subscribe(new Observer<ParticipantLapTimes>(lapTimes => {
+                try {
+                    Console.WriteLine(lapTimesStore.LoadAll().First(lt => lt.participantIndex.Equals(0)).lapTimes.Last().Value.lapTime);
+                } catch {
+
+                }
             }));
             rawHandler.Start();
             while(true){
