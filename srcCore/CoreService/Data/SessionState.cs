d174cae909a13414520b6f410a6be7fecf9636af
David Lusignan <davidl@vuwall.com>
2020-03-06 21:40:29 -0500

    Implement a SessionState object to be able to store a session ID

---
+++ srcCore/CoreService/Data/SessionState.cs
@@ -0,0 +1,38 @@
+﻿using CoreService.Storage;
+
+namespace CoreService.Data {
+    public class SessionState {
+        public Key SessionID { get; }
+        public SessionType SessionType { get; }
+        public SessionProgress SessionProgress { get; }
+        public SessionState(Key sessionID, SessionType sessionType, SessionProgress sessionProgress) {
+            SessionID = sessionID;
+            SessionType = sessionType;
+            SessionProgress = sessionProgress;
+        }
+
+        public override string ToString() {
+            return "SessionID: " + SessionID.ToString() + "; SessionType: " + SessionType + "; SessionProgress: " + SessionProgress;
+        }
+    }
+
+    public enum SessionType {
+        Invalid,
+        Practice,
+        Qualification,
+        Race,
+        Test,
+        FormationLap,
+        TimeAttack
+    }
+
+    public enum SessionProgress {
+        Invalid,
+        NotStarted,
+        Racing,
+        Finished,
+        Disqualified,
+        Retired,
+        DNF
+    }
+}
