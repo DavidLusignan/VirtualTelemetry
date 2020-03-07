da819065870ef030e142aae462b0e03bdf3e3e91
David Lusignan <davidl@vuwall.com>
2020-03-06 22:10:58 -0500

    Implement a ParticipantLapTimesStore that can observe participant lap times and store them

---
+++ srcCore/CoreService/Storage/ParticipantLapTimesStore.cs
@@ -0,0 +1,22 @@
+﻿using Global.Observable;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreService.Storage {
+    public class ParticipantLapTimesStore : CollectionStore<ParticipantLapTimes> {
+        private object _stateLock = new object();
+        public ParticipantLapTimesStore(LiteDatabase db) : base(db) { }
+
+        public void Observe(IObservable<ParticipantLapTimes> observable) {
+            observable.Subscribe(new Observer<ParticipantLapTimes>(OnState));
+        }
+
+        private void OnState(ParticipantLapTimes lapTimes) {
+            lock(_stateLock) {
+                Store(lapTimes);
+            }
+        }
+    }
+}
