efa66812ebb82080f2b66f9288aeb16190a30459
David Lusignan <davidl@vuwall.com>
2020-03-06 21:41:04 -0500

    Fix issue with parsing session and game state from PC2

--- srcCore/CoreService/UDPProjectCars2/PacketParser/Data/PC2GameStatePacket.cs
+++ srcCore/CoreService/UDPProjectCars2/PacketParser/Data/PC2GameStatePacket.cs
@@ -3,28 +3,27 @@
 using CoreService.UDPProjectCars2.RawPacketHandler;
 
 namespace CoreService.UDPProjectCars2.PacketParser.Data {
-    class PC2GameStatePacket : PC2BasePacket {
-        ushort buildVersionNumber { get; }
-        PC2GameState gameState { get; }
-        PC2SessionState sessionState { get; }
+    public class PC2GameStatePacket : PC2BasePacket {
+        public ushort buildVersionNumber { get; }
+        public PC2GameState gameState { get; }
+        public PC2SessionState sessionState { get; }
         public PC2GameStatePacket(PC2PacketMeta baseUDP, ushort buildVersionNumber, PC2GameState gameState, PC2SessionState sessionState) {
             this.baseUDP = baseUDP;
             this.buildVersionNumber = buildVersionNumber;
             this.gameState = gameState;
             this.sessionState = sessionState;
-            System.Console.WriteLine("GameState: " + gameState.ToString() + "; SessionState: " + sessionState.ToString());
         }
 
         public static PC2GameStatePacket Create(PC2RawPacket rawPacket, PC2PacketMeta meta) {
             var buildVersionNumber = rawPacket.Data.ReadUInt16();
             var mixedStates = rawPacket.Data.ReadByte();
-            var gameState = mixedStates >> 5;
-            var sessionState = mixedStates - (gameState << 5) >> 2;
+            var sessionState = mixedStates >> 4;
+            var gameState = mixedStates - (sessionState << 4);
             return new PC2GameStatePacket(meta, buildVersionNumber, (PC2GameState)gameState, (PC2SessionState)sessionState);
         }
     }
 
-    enum PC2SessionState {
+    public enum PC2SessionState {
         Invalid,
         Practice,
         Test,
@@ -34,7 +33,7 @@ namespace CoreService.UDPProjectCars2.PacketParser.Data {
         TimeAttack
     }
 
-    enum PC2GameState {
+    public enum PC2GameState {
         Exited,
         FrontEnd,
         IngamePlaying,
