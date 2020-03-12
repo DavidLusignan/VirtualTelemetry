27b48e7ba5586ccc3d7fc8168baf66b8ddb5fb17
David Lusignan <davidl@vuwall.com>
2020-03-11 20:37:06 -0400

    Implement a store for SessionTrackInfo

---
+++ srcCore/CoreService/Storage/SpecificStores/SessionTrackInfoStore.cs
@@ -0,0 +1,16 @@
+﻿using CoreService.UDPProjectCars2.StdDataConvertor;
+using Global.Observable;
+using LiteDB;
+using System;
+
+namespace CoreService.Storage.SpecificStores {
+    public class SessionTrackInfoStore : CollectionStore<SessionTrackInfo> {
+        public SessionTrackInfoStore(LiteDatabase db) : base(db) { }
+        public void Observe(IObservable<SessionTrackInfo> observable) {
+            observable.Subscribe(new Observer<SessionTrackInfo>(OnState));
+        }
+        private void OnState(SessionTrackInfo trackInfo) {
+            Upsert(trackInfo);
+        }
+    }
+}
