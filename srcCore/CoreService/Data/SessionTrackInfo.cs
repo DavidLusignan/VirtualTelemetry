c916397861b945d725b389b814a158657335d06c
David Lusignan <davidl@vuwall.com>
2020-03-11 20:32:55 -0400

    Implement a standard SessionTrackInfo

---
+++ srcCore/CoreService/Data/SessionTrackInfo.cs
@@ -0,0 +1,15 @@
+﻿using CoreService.Storage;
+using CoreService.Storage.DTOs;
+
+namespace CoreService.UDPProjectCars2.StdDataConvertor {
+    public class SessionTrackInfo : IStorable {
+        public Key Id { get; }
+        public string TrackName { get; }
+        public string TrackVariation { get; }
+        public SessionTrackInfo(Key id, string trackName, string trackVariation) {
+            Id = id;
+            TrackName = trackName;
+            TrackVariation = trackVariation;
+        }
+    }
+}
