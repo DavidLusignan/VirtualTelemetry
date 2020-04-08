c98dad11347c92c7009f5a2cfacbc857ea23664a
David Lusignan <davidl@vuwall.com>
2020-04-08 12:48:28 -0400

    Send out throttle notifications as a test for the front-end

--- srcCore/WebUI/Program.cs
+++ srcCore/WebUI/Program.cs
@@ -1,16 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using CoreService.UDPProjectCars2.PacketParser;
+using CoreService.UDPProjectCars2.RawPacketHandler;
+using CoreService.UDPProjectCars2.StdDataConvertor;
+using Global.Observable;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebUI {
     public class Program {
         public static void Main(string[] args) {
+            Task.Run(() => Test());
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -18,5 +22,17 @@ namespace WebUI {
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void Test() {
+            var rawHandler = PC2RawHandler.Create(5606);
+            var packetParser = new PC2PacketParser(rawHandler);
+            var throttlePipeline = new PC2ThrottlePositionPipeline(packetParser);
+            throttlePipeline.Subscribe(new Observer<ThrottlePosition>(OnThrottle));
+            rawHandler.Start();
+        }
+
+        private static void OnThrottle(ThrottlePosition throttle) {
+            Startup.TestHub.Clients.All.SendAsync("ReceiveMessage", throttle.Value);
+        }
     }
 }
